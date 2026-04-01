13c98ccd0c10dfdd0e80e282c3ba32f585507982
Duy Phạm <duypham@MacBook-Pro-8.local>
2026-04-01 14:42:14 +0700

    Integrate CoordinatesSearch to sample app

--- composeApp/src/androidMain/kotlin/com/what3words/search/wrapper/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/what3words/search/wrapper/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.what3words.androidwrapper.datasource.text.W3WApiTextDataSource
+import com.what3words.search.wrapper.coordinates.CoordinatesSearch
+import com.what3words.search.wrapper.core.W3WSearchClient
 
 class MainActivity : ComponentActivity() {
 
@@ -14,12 +16,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val textDataSource = W3WApiTextDataSource.create(this, BuildConfig.W3W_WRAPPER_API_KEY)
+        val searchClient = W3WSearchClient(textDataSource) {
+            install(CoordinatesSearch, 10)
+        }
 
         setContent {
             App(
                 viewModel = viewModel<SearchViewModel>(
                     factory = SearchViewModel.Factory(
-                        textDataSource = textDataSource,
+                        searchClient
                     )
                 )
             )
