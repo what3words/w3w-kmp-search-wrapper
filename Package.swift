eddc457f2a297f8314449d053bf71f0888c3296d
Anka <runner@sjc20-cw711-092d74be-c79c-45a4-a00a-59377bd38167-F6AD9692A28F.local>
2026-07-21 22:42:53 +0000

    KMP SPM package release for 1.0.0-SNAPSHOT

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/what3words/w3w-kmp-search-wrapper/releases/assets/485232171.zip"
+let remoteKotlinChecksum = "ef0f1f47e843fd29fad526f32c308366327f6bad52e81f0f41ce9ef43b9bd95c"
+let packageName = "W3WKotlinSearchWrapper"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
