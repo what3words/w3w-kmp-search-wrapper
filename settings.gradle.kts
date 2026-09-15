2df8000a9b884be642ee09a35239f3fea108773c
Nghia Nguyen <nghia.nguyen@what3words.com>
2026-09-15 16:58:18 +0700

    override w3w-android-design-library 2.2.0-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,8 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("com.what3words:android-version-catalog:2026.09.02-SNAPSHOT")
+            from("com.what3words:android-version-catalog:2026.09.01")
+            version("w3w-android-design-library", "2.2.0-SNAPSHOT")
         }
     }
 }
